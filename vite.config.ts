75fcfca7ee38049157e8bed2e8447011f08bce07
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-03-05 23:49:49 -0500

    implementacion de tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'    // plugin de tailwindcss
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
