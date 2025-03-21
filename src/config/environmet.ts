971250fa3f9ad08cb5fdaea6ec972d4d9a7650cd
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-03-21 18:18:33 -0500

    feat: actualizar la configuración del entorno; agregar dominio y almacenamiento S3

--- src/config/environmet.ts
+++ src/config/environmet.ts
@@ -1,5 +1,5 @@
-export const environment = {
-    baseUrl: "https://dev.domiduck.com/store",
-  };
-  
-  
+export const enviroment = {
+    baseUrl : 'https://dev.domiduck.com/store',
+    domain: '#',
+    s3Storage : 'https://bucket-domiduck-s3.s3.amazonaws.com/'
+}
