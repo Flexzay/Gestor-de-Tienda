93e4f5d72ac3c52b4375513f62821dabedf74a6e
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-03-23 01:54:24 -0500

    feat: reemplazar imagen de domiduck.png por domiduck.svg en DashboardImageCard

--- src/components/dashboard/DashboardImageCard.tsx
+++ src/components/dashboard/DashboardImageCard.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-import domiduck from ""; // ✅ Importamos la imagen correctamente
+import Domiduck from "../../assets/img/domiduck.svg"; // ✅ Importamos la imagen correctamente
 
 const DashboardImageCard: React.FC<{ title: string; value: string }> = ({ title, value }) => (
   <div
     className="relative bg-cover bg-center rounded-xl shadow-xl p-6 transition-all duration-300 hover:shadow-2xl hover:-translate-y-2 flex flex-col justify-between min-w-0 overflow-hidden border border-gray-200 dark:border-gray-700 h-48"
-    style={{ backgroundImage: `url(${domiduck})` }} // ✅ Usamos la imagen importada
+    style={{ backgroundImage: `url(${Domiduck})` }} // ✅ Usamos la imagen importada
   >
     {/* Capa oscura opcional */}
     <div className="absolute inset-0  bg-opacity-20 rounded-xl"></div>
 
-    {/* Contenido de la card */}
-    <div className="relative z-10 text-center text-white">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-4xl font-bold">{value}</p>
-    </div>
+    
   </div>
 );
 
