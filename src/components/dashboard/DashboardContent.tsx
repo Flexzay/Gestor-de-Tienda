ff9d96a132cbd93aa83211cb7b6fc6374d500287
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-04-09 22:55:07 -0500

    feat: eliminar espacios innecesarios en el componente DashboardContent

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -46,8 +46,6 @@ const DashboardContent: React.FC = () => {
 
   return (
     <main className="p-6 bg-gray-100 w-full min-h-[100vh] overflow-y-scroll md:ml-72">
-
-
       <button
         onClick={() => setIsAdding(true)}
         className="mb-4 px-6 py-3 text-xl font-semibold text-white bg-[#ff204e] rounded-lg hover:bg-[#ff3b61] transition-all duration-300"
