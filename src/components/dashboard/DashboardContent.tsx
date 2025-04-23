7322940d1bff37782d817f7f1e6222660dbf3f69
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-04-22 21:48:39 -0500

    feat: eliminar importación innecesaria de useEffect en DashboardContent

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AddProductForm from "./Product/Product";
 import DashboardCard from "./DashboardCard";
 import DashboardImageCard from "./DashboardImageCard";
