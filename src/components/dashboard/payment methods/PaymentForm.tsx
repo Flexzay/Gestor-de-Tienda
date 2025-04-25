fdaad87c272a43fae9afc89dfc69626ac409201e
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-04-25 17:09:03 -0500

    feat: simplificar PaymentForm eliminando código innecesario y optimizando importaciones

--- src/components/dashboard/payment methods/PaymentForm.tsx
+++ src/components/dashboard/payment methods/PaymentForm.tsx
@@ -1,20 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Plus, CreditCard } from "lucide-react";
-import { environment } from "../../../config/environmet";
 
 // Utiliza esta función para convertir Base64 a archivo
-const dataURLtoFile = (dataUrl: string, fileName: string) => {
-  const arr = dataUrl.split(",");
-  const mimeMatch = arr[0].match(/:(.*?);/);
-  const mime = mimeMatch ? mimeMatch[1] : "";
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], fileName, { type: mime });
-};
 
 interface PaymentFormProps {
   formData: any;
